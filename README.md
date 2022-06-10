f142360806a70db0d638dea8e156f7a694079e4f
REMAX-SOURCE <107276529+REMAX-SOURCE@users.noreply.github.com>
2022-06-10 23:13:40 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+https://t.me/REMAX_SOURCE
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/REMAX-SOURCE/REMAX)
---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "Video Stream")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_NAME = getenv("OWNER_NAME", "hama")
+ALIVE_NAME = getenv("ALIVE_NAME", "hama")
+BOT_USERNAME = getenv("BOT_USERNAME", "Remax_Musicbot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "SoSoMuSiC55")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "CU_94")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "REMAX_SOURCE")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/06b651dc0d827b6887764.jpg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/REMAX-SOURCE/REMAX")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/be59e08410179632a7888.jpg")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/be59e08410179632a7888.jpg")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/be59e08410179632a7888.jpg")
